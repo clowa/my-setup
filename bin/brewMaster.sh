1da9b72958e9081e59ea5b29e8b9594fffdd99a7
Cedric Ahlers <55838528+clowa@users.noreply.github.com>
2022-09-14 20:01:31 +0200

    add brewMaster

---
+++ bin/brewMaster.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+cat << EOF
+██████╗ ██████╗ ███████╗██╗    ██╗    ███╗   ███╗ █████╗ ███████╗████████╗███████╗██████╗ 
+██╔══██╗██╔══██╗██╔════╝██║    ██║    ████╗ ████║██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗
+██████╔╝██████╔╝█████╗  ██║ █╗ ██║    ██╔████╔██║███████║███████╗   ██║   █████╗  ██████╔╝
+██╔══██╗██╔══██╗██╔══╝  ██║███╗██║    ██║╚██╔╝██║██╔══██║╚════██║   ██║   ██╔══╝  ██╔══██╗
+██████╔╝██║  ██║███████╗╚███╔███╔╝    ██║ ╚═╝ ██║██║  ██║███████║   ██║   ███████╗██║  ██║
+╚═════╝ ╚═╝  ╚═╝╚══════╝ ╚══╝╚══╝     ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
+EOF
+echo 'Updating brew ...'
+brew update > /dev/null
+
+echo 'Outdated packages:'
+brew outdated --quiet
+
+echo 'Upgrading packages ...'
+brew upgrade && brew upgrade hashicorp/tap/packer
+
+echo 'Upgrading casks ...'
+brew upgrade --cask
+
+echo 'Cleanup ...'
+brew autoremove && brew cleanup
+
+# Upgrading NVM via gir repo. It not necessary since nvm is installed via homebrew
+# echo 'Upateing nvm ...'
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | zsh
+
+exit 0
