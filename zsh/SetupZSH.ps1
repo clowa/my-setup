456d2f8f2068f6fdc4a43a7be396282ab1472ee0
Cedric Ahlers <55838528+clowa@users.noreply.github.com>
2022-09-14 20:28:31 +0200

    force creation of symlinc

--- zsh/SetupZSH.ps1
+++ zsh/SetupZSH.ps1
@@ -4,7 +4,7 @@ $configGitRepoPath = if (Test-Path "$HOME/github/my-setup") { "$HOME/github/my-s
 
 
 if (Get-ShouldOverwrite -Prompt "File ~/.zshrc is present. Do you want to overwrite? (y/n)" -Path $HOME/.zshrc) {
-    New-Item -ItemType SymbolicLink -Path $HOME/.zshrc -Target $configGitRepoPath/zsh/.zshrc
+    New-Item -ItemType SymbolicLink -Path $HOME/.zshrc -Target $configGitRepoPath/zsh/.zshrc -Force
 }
 
 New-Item -ItemType SymbolicLink -Path $HOME/.zsh -Target $configGitRepoPath/zsh/.zsh/
